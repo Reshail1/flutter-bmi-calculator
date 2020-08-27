59515154776c6b467fb04388c7942bb0b3e6973b
Reshail <reshailawan@gmail.com>
2020-08-27 16:20:41 +0500

    Disabled Debug Button

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: InputPage(),
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
